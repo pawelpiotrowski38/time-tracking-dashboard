d4fc1059e9eaa9e12fb9bb5735f54f322073da12
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-11-17 20:40:09 +0100

    Change fill-rule to fillRule in svg path tag

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -20,7 +20,7 @@ export default function Activity({ activity, timeframe }) {
                     </h2>
                     <button className='activity__button'>
                         <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
+                            <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fillRule="evenodd"/>
                         </svg>
                     </button>
                 </div>
