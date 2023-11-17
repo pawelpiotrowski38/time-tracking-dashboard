60fcfa4ec1b9c058efaa6c634bf047ef41d51eb7
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-11-17 19:54:33 +0100

    Add loading

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,39 @@
 import { useEffect, useState } from 'react';
 import User from './User';
 import Activities from './Activities';
+import Loading from './Loading';
 import './dashboard.css';
 
 export default function Dashboard() {
     const [activities, setActivities] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [timeframe, setTimeframe] = useState('weekly');
 
     useEffect(() => {
         const fetchData = async function() {
-            const response = await fetch('../data.json');
-            const data = await response.json();
-            setActivities(data);
+            try {
+                const response = await fetch('../data.json');
+                const data = await response.json();
+                setActivities(data);
+            } catch(error) {
+                console.log(error);
+            } finally {
+                setIsLoading(false);
+            }
         }
         fetchData();
     }, []);
 
     return (
         <section className='dashboard'>
-            <User timeframe={timeframe} onSetTimeframe={setTimeframe} />
-            <Activities activities={activities} timeframe={timeframe} />
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <>
+                    <User timeframe={timeframe} onSetTimeframe={setTimeframe} />
+                    <Activities activities={activities} timeframe={timeframe} />
+                </>
+            )}
         </section>
     )
 }
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import './loading.css';
+
+export default function Loading() {
+    return (
+        <div className='loading'>
+            <div className="loading__spinner"></div>
+        </div>
+    )
+}
