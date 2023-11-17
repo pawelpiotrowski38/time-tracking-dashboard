124772d52041e4ac58fa40eff1370cb697571cc1
Paweł Piotrowski <pawel.piotrowski38@gmail.com>
2023-11-17 20:33:22 +0100

    Change ellipsis icon svg rendering

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -19,7 +19,9 @@ export default function Activity({ activity, timeframe }) {
                         {activity.title}
                     </h2>
                     <button className='activity__button'>
-                        <img className='activity__button-icon' src='./images/icon-ellipsis.svg' alt='ellipsis' />
+                        <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/>
+                        </svg>
                     </button>
                 </div>
                 <div className='activity__data'>
